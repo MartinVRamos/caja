61309858b288db4f21364b8c17ac72135ecec652
MartinVRamos <112112495+MartinVRamos@users.noreply.github.com>
2026-08-08 19:35:09 -0400

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,17 @@
-// REEMPLAZA estos valores por los de tu proyecto Firebase.
-// No pongas contraseñas aquí. Esta configuración de Firebase para Web es pública por diseño.
-// Después configura las reglas de Firestore para permitir únicamente las operaciones que necesites.
-export const firebaseConfig = {
-  apiKey: "TU_API_KEY",
-  authDomain: "TU_PROYECTO.firebaseapp.com",
-  projectId: "TU_PROJECT_ID",
-  storageBucket: "TU_PROYECTO.firebasestorage.app",
-  messagingSenderId: "TU_SENDER_ID",
-  appId: "TU_APP_ID"
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCJMgDhB4Yb10FCnwzMSYM16Dq-kSVu2UI",
+  authDomain: "caja-bingo.firebaseapp.com",
+  projectId: "caja-bingo",
+  storageBucket: "caja-bingo.firebasestorage.app",
+  messagingSenderId: "936402671673",
+  appId: "1:936402671673:web:eba7b3b85b444b634955e8"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
