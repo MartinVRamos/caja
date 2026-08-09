4b5f21b3dcf11b91d86e133db72d480d4b130e62
MartinVRamos <112112495+MartinVRamos@users.noreply.github.com>
2026-08-08 20:08:43 -0400

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,4 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
+export const firebaseConfig = {
   apiKey: "AIzaSyCJMgDhB4Yb10FCnwzMSYM16Dq-kSVu2UI",
   authDomain: "caja-bingo.firebaseapp.com",
   projectId: "caja-bingo",
@@ -12,6 +6,3 @@ const firebaseConfig = {
   messagingSenderId: "936402671673",
   appId: "1:936402671673:web:eba7b3b85b444b634955e8"
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
